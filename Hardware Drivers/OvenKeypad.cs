3f29cacd45be8debb50ce1d2e7d322687db26413
Sukasa <nick.m@shaw.ca>
2015-04-05 19:47:11 -0700

    Fix Keypad
    * Fixes buzzer to be PWM
    * Corrects incorrect pin bit assignments
    * Adds KeyPress / KeyDown distinction
    * Better LED control

--- Hardware Drivers/OvenKeypad.cs
+++ Hardware Drivers/OvenKeypad.cs
@@ -1,8 +1,9 @@
 using System;
+using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
-using Microsoft.SPOT;
+
 using Microsoft.SPOT.Hardware;
 using SecretLabs.NETMF.Hardware;
 using SecretLabs.NETMF.Hardware.Netduino;
@@ -14,71 +15,121 @@ namespace Reflow_Oven_Controller
         private TristatePort[] _Rows;
         private InputPort[] _Columns;
         private OutputPort _LED;
-        private OutputPort _Buzzer;
+        private PWM _Buzzer;
         private Thread _LEDThread;
+
 
         [Flags()]
         public enum Keys
         {
             None = 0,
-            Bake = 1,
-            Broil = 2,
-            Toast = 4,
-            Warm = 8,
-            Temperature = 16,
-            Time = 32,
-            Up = 64,
+            Any = 65535,
+
+            Stop = 512,
+            Start = 256,
             Down = 128,
-            Stop = 256,
-            Start = 512
+            Up = 64,
+            Warm = 32,
+            Temp = 16,
+            Time = 8,
+            Bake = 4,
+            Broil = 2,
+            Toast = 1
         }
         public enum LEDState {
             Off = 0,
-            Flashing = 1,
+            SlowFlash = 450,
+            FastFlash = 120,
             On = 2
         }
 
+        public Keys KeysDown { get; set; }
         public Keys KeysPressed { get; private set; }
+
         public bool IsKeyPressed(Keys Key)
         {
             return (KeysPressed & Key) == Key;
         }
 
-        public LEDState LEDControl { get; set; }
-        public bool Buzzer
+        public bool IsKeyDown(Keys Key)
         {
-            get
-            {
-                return _Buzzer.Read();
-            }
-            set
-            {
-                _Buzzer.Write(value);
-            }
+            return (KeysDown & Key) == Key;
         }
 
-        public void LEDThread() {
-            while (true) {
-                Thread.Sleep(310);
+        public LEDState LEDControl { get; set; }
 
-                switch (LEDControl)
+        
+        public void Beep(BeepLength Length)
+        {
+            _BeepTimeLeft = Math.Max(_BeepTimeLeft, (int)Length);
+            _Buzzer.Start();
+        }
+
+        public enum BeepLength : int
+        {
+            Long = 1600,
+            Medium = 650,
+            Short = 250
+        }
+
+        private int _BeepTimeLeft;
+        private int _LEDTimeLeft;
+
+        public void KeypadThread() {
+            _LEDTimeLeft = 310;
+
+            int SleepTime;
+
+            while (true) {
+                if (_BeepTimeLeft <= 0)
                 {
-                    case LEDState.On:
-                        _LED.Write(true);
-                        break;
-                    case LEDState.Flashing:
-                        _LED.Write(!_LED.Read());
-                        break;
-                    default:
-                        _LED.Write(false);
-                        break;
+                    SleepTime = _LEDTimeLeft;
                 }
+                else
+                {
+                    SleepTime = Math.Min(_LEDTimeLeft, _BeepTimeLeft);
+                }
+
+                SleepTime = Math.Min(50, SleepTime);
+
+                Thread.Sleep(SleepTime);
+
+                _LEDTimeLeft -= SleepTime;
+                if (_LEDTimeLeft <= 0)
+                {
+                    _LEDTimeLeft = 310;
+                    switch (LEDControl)
+                    {
+                        case LEDState.On:
+                            _LED.Write(true);
+                            break;
+                        case LEDState.Off:
+                            _LED.Write(false);
+                            break;
+                        default:
+                            _LEDTimeLeft = (int)LEDControl;
+                            _LED.Write(!_LED.Read());
+                            break;
+                    }
+                }
+
+
+                if (_BeepTimeLeft > 0)
+                {
+                    _BeepTimeLeft -= SleepTime;
+                    if (_BeepTimeLeft <= 0)
+                    {
+                        _BeepTimeLeft = 0;
+                        _Buzzer.Stop();
+                    }
+                }
+
             }
         }
 
         public OvenKeypad(Cpu.Pin RowPin1, Cpu.Pin RowPin2, Cpu.Pin RowPin3, Cpu.Pin RowPin4,
                           Cpu.Pin ColumnPin1, Cpu.Pin ColumnPin2, Cpu.Pin ColumnPin3,
-                          Cpu.Pin LEDPin, Cpu.Pin BuzzerPin)
+                          Cpu.Pin LEDPin, Cpu.PWMChannel BuzzerPin)
         {
             _Rows = new TristatePort[4];
             _Columns = new InputPort[3];
@@ -97,11 +148,11 @@ namespace Reflow_Oven_Controller
             _Columns[2] = new InputPort(ColumnPin3, false, Port.ResistorMode.PullDown);
             
             _LED = new OutputPort(LEDPin, true);
-            _Buzzer = new OutputPort(BuzzerPin, false);
+            _Buzzer = new PWM(BuzzerPin, 2300.0, 0.5, false);
 
-            _LEDThread = new Thread(LEDThread);
+            _LEDThread = new Thread(KeypadThread);
             _LEDThread.Start();
-            LEDControl = LEDState.Flashing;
+            LEDControl = LEDState.Off;
         }
 
         public void Scan()
@@ -120,15 +171,20 @@ namespace Reflow_Oven_Controller
                     if (_Columns[Column].Read())
                     {
                         Buffer |= (Keys)Flag;
-                        Flag <<= 1;
                     }
+                    Flag <<= 1;
                 }
                 _Rows[Row].Write(false);
                 _Rows[Row].Active = false;
             }
             if (NumKeys < 3) // Anything more than 3 is potentially misreading, so don't store
             {   // A misread of 3 buttons as more will show up as 4 or more here.  Thus, reading 3 buttons can only happen if it's not a misread.
-                KeysPressed = Buffer;
+                KeysPressed = (Buffer ^ KeysDown) & Buffer;
+                KeysDown = Buffer;
+                if (KeysPressed != Keys.None)
+                {
+                    Beep(BeepLength.Short);
+                }
             }
         }
     }
