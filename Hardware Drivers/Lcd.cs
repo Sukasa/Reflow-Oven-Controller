966965e1c789a0754d73739f7ad8a7e03e28b45f
Sukasa <nick.m@shaw.ca>
2015-04-18 16:08:28 -0700

    Update Lcd initializer to meet spec

--- Hardware Drivers/Lcd.cs
+++ Hardware Drivers/Lcd.cs
@@ -15,12 +15,14 @@ namespace Reflow_Oven_Controller.Hardware_Drivers
         private byte[] _Buffer;
         private SPI.Configuration _Device;
         private OutputPort _ChipSelect;
+        private OutputPort _DataCommand;
         private PWM _Backlight;
 
-        public Lcd(Cpu.Pin ChipSelectPin, Cpu.PWMChannel BacklightPin)
+        public Lcd(Cpu.Pin ChipSelectPin, Cpu.Pin DataCommandPin, Cpu.PWMChannel BacklightPin)
         {
             _Device = SPIBus.Instance().CreateBusDevice(Pins.GPIO_NONE, false, 10000);
             _ChipSelect = new OutputPort(ChipSelectPin, true);
+            _DataCommand = new OutputPort(DataCommandPin, true);
             _Backlight = new PWM(BacklightPin, 2000, 1.0, false);
 
             _Buffer = new byte[32768];
@@ -77,31 +79,47 @@ namespace Reflow_Oven_Controller.Hardware_Drivers
 
         private void LoadExecuteInitSequence()
         {
-            using (FileStream Stream = new FileStream("\\SD\\Oven\\LCDInit", FileMode.Open))
+            using (FileStream Stream = new FileStream("\\SD\\Oven\\LCDInit.bin", FileMode.Open))
             {
                 int Length = Stream.Read(_Buffer, 0, 32768);
                 SPIBus Bus = SPIBus.Instance();
                 Bus.SelectDevice(_Device);
 
+                // CHANGE: Reset CS between all bytes
+                // CHANGE: Set D/C (A2) *low* for commands, *high* for data
+
                 for (int Ptr = 0; Ptr < Length; )
                 {
                     // Get number of bytes in data portion of packet
                     int DataLen = _Buffer[Ptr];
+
+                    // 0xFF means sleep, 0 params
+                    if (DataLen == 255)
+                    {
+                        DataLen = 0;
+                        Thread.Sleep(120);
+                    }
                     Ptr++;
 
                     // Open packet
                     _ChipSelect.Write(false); // Active low
+                    _DataCommand.Write(false);
 
                     // Write command byte of packet
                     Bus.Write(_Buffer, Ptr, 1);
                     Ptr++;
-                    
-                    // Write data bytes of packet
-                    Bus.Write(_Buffer, Ptr, DataLen);
-                    Ptr += DataLen;
 
-                    // Close packet
+                    // Prep for data writes
                     _ChipSelect.Write(true); // Active low
+                    _DataCommand.Write(true);
+
+                    // Write data bytes of packet
+                    for (int DPtr = Ptr; DataLen > 0; DPtr++, Ptr++, DataLen--)
+                    {
+                        _ChipSelect.Write(false); // Active low
+                        Bus.Write(_Buffer, DPtr, 1);
+                        _ChipSelect.Write(true); // Active low
+                    }
                 }
 
             }
