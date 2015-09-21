554a4af9121206410e1d2bf0bd4f8b9cc6fe24ca
Sukasa <nick.m@shaw.ca>
2015-09-21 14:17:37 -0700

    Minor bug fixes in ProfileDatapoint struct

--- Process Control/ProfileDatapoint.cs
+++ Process Control/ProfileDatapoint.cs
@@ -5,11 +5,12 @@ namespace Reflow_Oven_Controller.Process_Control
 {
     struct ProfileDatapoint
     {
+        //seconds,temperature,flags
         public TimeSpan TimeOffset;
         public float Temperature;
         public DatapointFlags Flags;
 
-        public const int Stride = 5;
+        public const int Stride = 9;
 
         [Flags]
         public enum DatapointFlags
@@ -20,6 +21,7 @@ namespace Reflow_Oven_Controller.Process_Control
             WaitForTemperature = 4, // Pause the process timer until setpoint is reached
             Beep = 8, // Beep once when this datapoint becomes active
             InsertItemNotification = 16, // IfNoAbortDoorOpen is set, this selects between "insert item now" and "item ready" notifications
+            Cooling = 32 // Say 'Cooling' instead of 'Baking'
         }
 
         public ProfileDatapoint(TimeSpan TimePoint, float Temp)
@@ -29,17 +31,30 @@ namespace Reflow_Oven_Controller.Process_Control
             Flags = DatapointFlags.None;
         }
 
+        public ProfileDatapoint(TimeSpan TimePoint, float Temp, DatapointFlags Flags)
+            : this(TimePoint, Temp)
+        {
+            this.Flags = Flags;
+        }
+
+        public ProfileDatapoint(int Seconds, float Temp, DatapointFlags Flags)
+        {
+            TimeOffset = new TimeSpan(0, 0, Seconds);
+            Temperature = Temp;
+            this.Flags = Flags;
+        }
+
         public void ToBytes(byte[] OutputBuffer, int Offs) {
             Array.Copy(BitConverter.GetBytes(TimeOffset.Seconds + (TimeOffset.Minutes * 60) + (TimeOffset.Hours * 3600) + (TimeOffset.Days * 86400)), 0, OutputBuffer, Offs, 4);
             Array.Copy(BitConverter.GetBytes(Temperature), 0, OutputBuffer, Offs + 4, 4);
-            OutputBuffer[Offs + 5] = (byte)Flags;
+            OutputBuffer[Offs + 8] = (byte)Flags;
         }
 
         public ProfileDatapoint(byte[] Buffer, int Offs)
         {
             TimeOffset = new TimeSpan(0, 0, BitConverter.ToInt32(Buffer, Offs));
             Temperature = BitConverter.ToSingle(Buffer, Offs + 4);
-            Flags = (DatapointFlags)Buffer[Offs + 5];
+            Flags = (DatapointFlags)Buffer[Offs + 8];
         }
     }
 }
