077a8859a635fcd9925d48f54740b9b24403df05
Sukasa <nick.m@shaw.ca>
2015-09-22 20:23:43 -0700

    Implement live data in Web GUI

--- WWW/Default.js
+++ WWW/Default.js
@@ -3,7 +3,7 @@ function init() {
     $("#savefile").click(function (Event) { doFileUpload(); });
     $("#deletefile").click(function (Event) { deleteFile(); });
 
-    //setInterval(updateStats, 2500);
+    setInterval(updateStats, 2500);
 }
 
 var konami = [112, 114, 105, 115, 99, 105, 108, 108, 97];
@@ -20,20 +20,36 @@ function kCode(Event) {
         if (kStep == konami.length) {
             $(".pulldown").animate({top: "-2px"}, 1000);
         }
-    } else {
+    } else if (kStep < konami.length) {
         kStep = 0;
     }
+}
+
+function FormatNum(Number, Suffix) {
+    Number += '';
+    var x = nStr.split('.');
+    var x1 = x[0];
+    var x2 = x.length > 1 ? '.' + x[1] : '';
+    var rgx = /(\d+)(\d{3})/;
+    while (rgx.test(x1)) {
+        x1 = x1.replace(rgx, '$1' + ',' + '$2');
+    }
+    return x1 + x2 + Suffix;
 }
 
 function updateStats() {
     // Download stats from /Data
 
-    var Request = new XMLHttpRequest();
+    $.getJSON("Data", function (Data) {
+        $("OvenTemp").text(FormatNum(Data.OvenTemperature, " °C") + " (" + FormatNum(Data.TSense1, " °C") + ", " + FormatNum(Data.TSense2, " °C"));
+        $("BayTemp").text(FormatNum(Data.BayTemperature), " °C");
+        $("FreeMem").text(FormatNum(Data.FreeMem, " B"));
+        $("DoorAjar").text(Data.DoorAjar ? "Ajar" : "Closed");
+        $("ElementStatuses").text(FormatNum(Data.LowerPower, "%") + ", " + FormatNum(Data.UpperPower, "%"));
+        $("CPULoad").text(FormatNum(Data.Load, "%"));
+        $("OvenFan").text(FormatNum(Data.Fan2, "%"));
+    });
 
-    Request.open("GET", "Data", false);
-    Request.send();
-
-    // Update stats pane
 }
 
 function deleteFile() {
