10f920ec1dd184c02231b87a7ac335dca567936f
Sukasa <nick.m@shaw.ca>
2016-03-09 13:08:07 -0800

    Web page improvements
    * Clean up unused javascript functions, and create minified version
    * Use minified version in HTML
    * Expand data window slightly to prevent a line break

--- WWW/Default.js
+++ WWW/Default.js
@@ -1,7 +1,5 @@
 function init() {
     $(document).keypress(function (Event) { kCode(Event); });
-    $("#savefile").click(function (Event) { doFileUpload(); });
-    $("#deletefile").click(function (Event) { deleteFile(); });
 
     setInterval(updateStats, 2500);
 }
@@ -27,7 +25,7 @@ function kCode(Event) {
 
 function FormatNum(Number, Suffix) {
     Number += '';
-    var x = nStr.split('.');
+    var x = Number.split('.');
     var x1 = x[0];
     var x2 = x.length > 1 ? '.' + x[1] : '';
     var rgx = /(\d+)(\d{3})/;
@@ -50,48 +48,4 @@ function updateStats() {
         $("OvenFan").text(FormatNum(Data.Fan2, "%"));
     });
 
-}
-
-function deleteFile() {
-    var fileName = $("#path").val();
-    var Request = new XMLHttpRequest();
-    Request.open("GET", "File/Delete", false);
-    Request.send("f=" + fileName);
-}
-
-function doFileUpload() {
-    var file = $("#file")[0].files[0];
-
-    var reader = new FileReader();
-    reader.onload = function (evt) {
-        var fileData = evt.target.result;
-        var fileName = $("#path").val();
-        var bytes = new Uint8Array(fileData);
-        var binaryText = '';
-        var fullText = '';
-
-        var chunksize = 0;
-
-        for (var index = 0; index < bytes.byteLength; index++) {
-            binaryText += String.fromCharCode(bytes[index]);
-            chunksize++;
-
-            if (chunksize == 256 || index >= bytes.byteLength - 1) {
-                chunksize = 0;
-                fullText += binaryText;
-                binaryText = btoa(binaryText);
-
-                var Request = new XMLHttpRequest();
-                Request.open("POST", "File/Write", false);
-                Request.send("f=" + fileName + "&d=" + btoa(binaryText));
-
-                binaryText = "";
-            }
-        }
-
-        console.log("Full Base64 is: ");
-        console.log(btoa(fullText));
-
-    };
-    reader.readAsArrayBuffer(file);
 }
